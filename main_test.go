7ff4fc38e752a41af9c19271ded799f03fdd638a
Alexander Trakhimenok <alex@trakhimenok.com>
2026-01-08 12:36:06 +0000

    fix: TestMainFunc - assert os.Stderr

--- main_test.go
+++ main_test.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"io"
+	"os"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -18,14 +22,29 @@ func TestMainFunc(t *testing.T) {
 	t.Run("getCommand_nil", func(t *testing.T) {
 		getCommand = func() *cli.Command { return nil }
 		osExitBackup := osExit
+		osStdErrBackup := os.Stderr
+		r, w, err := os.Pipe()
+		if err != nil {
+			t.Fatal(err)
+		}
+		os.Stderr = w
 		defer func() {
 			osExit = osExitBackup
+			os.Stderr = osStdErrBackup
 		}()
 		var exitCode int
 		osExit = func(i int) {
 			exitCode = i
 		}
+
 		main()
+
 		assert.Equal(t, 1, exitCode)
+		{
+			_ = w.Close()
+			var buf bytes.Buffer
+			_, _ = io.Copy(&buf, r)
+			assert.True(t, strings.Contains(buf.String(), "invalid memory address or nil pointer dereference"))
+		}
 	})
 }

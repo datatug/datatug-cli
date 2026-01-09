d2466186859bca20b27c674ce6c70c47f759872d
Alexander Trakhimenok <alex@trakhimenok.com>
2026-01-09 01:44:59 +0000

    fix: add breadcrumbs to filetug navigator

--- apps/datatugapp/commands/cmd_filetug.go
+++ apps/datatugapp/commands/cmd_filetug.go
@@ -4,6 +4,7 @@ import (
 	"github.com/datatug/datatug-cli/apps/datatugapp/datatugui/dtviewers"
 	"github.com/datatug/datatug-cli/pkg/filetug"
 	"github.com/datatug/datatug-cli/pkg/sneatview/sneatnav"
+	"github.com/datatug/datatug-cli/pkg/sneatview/sneatv"
 	"github.com/rivo/tview"
 )
 
@@ -18,7 +19,14 @@ func RegisterAsViewer() {
 	})
 }
 
-func goFilesManager(tui *sneatnav.TUI, focusTo sneatnav.FocusTo) error {
+func goFileTug(tui *sneatnav.TUI, focusTo sneatnav.FocusTo) error {
+	breadcrumbs := tui.Header.Breadcrumbs()
+	breadcrumbs.Clear()
+	breadcrumbs.Push(sneatv.NewBreadcrumb("Viewers", func() error {
+		return dtviewers.GoViewersScreen(tui, sneatnav.FocusToContent)
+	}))
+	breadcrumbs.Push(sneatv.NewBreadcrumb("FileTug", nil))
+
 	n := filetug.NewNavigator(tui.App, filetug.OnMoveFocusUp(func(source tview.Primitive) {
 		tui.Header.SetFocus(sneatnav.ToBreadcrumbs, source)
 	}))
